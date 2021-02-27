bda7950dad49a95c4d5c3021b99a4d10d26a6233
Austin Stoker <austin@austinstoker.com>
2021-02-27 13:34:47 -0700

    don't get the author or the like/reply button text.
    also remove a bunch of commented out code

--- scraper.py
+++ scraper.py
@@ -17,187 +17,6 @@ with open('facebook_credentials.txt') as file:
     EMAIL = file.readline().split('"')[1]
     PASSWORD = file.readline().split('"')[1]
 
-
-# def _extract_post_text(item):
-#     actualPosts = item.find_all(attrs={"data-testid": "post_message"})
-#     text = ""
-#     if actualPosts:
-#         for posts in actualPosts:
-#             paragraphs = posts.find_all('p')
-#             text = ""
-#             for index in range(0, len(paragraphs)):
-#                 text += paragraphs[index].text
-#     return text
-
-
-# def _extract_link(item):
-#     postLinks = item.find_all(class_="_6ks")
-#     link = ""
-#     for postLink in postLinks:
-#         link = postLink.find('a').get('href')
-#     return link
-
-
-# def _extract_post_id(item):
-#     postIds = item.find_all(class_="_5pcq")
-#     post_id = ""
-#     for postId in postIds:
-#         post_id = f"https://www.facebook.com{postId.get('href')}"
-#     return post_id
-
-
-# def _extract_image(item):
-#     postPictures = item.find_all(class_="scaledImageFitWidth img")
-#     image = ""
-#     for postPicture in postPictures:
-#         image = postPicture.get('src')
-#     return image
-
-
-# def _extract_shares(item):
-#     postShares = item.find_all(class_="_4vn1")
-#     shares = ""
-#     for postShare in postShares:
-
-#         x = postShare.string
-#         if x is not None:
-#             x = x.split(">", 1)
-#             shares = x
-#         else:
-#             shares = "0"
-#     return shares
-
-
-# def _extract_comments(item):
-#     postComments = item.findAll("div", {"class": "_4eek"})
-#     comments = dict()
-#     # print(postDict)
-#     for comment in postComments:
-#         if comment.find(class_="_6qw4") is None:
-#             continue
-
-#         commenter = comment.find(class_="_6qw4").text
-#         comments[commenter] = dict()
-
-#         comment_text = comment.find("span", class_="_3l3x")
-
-#         if comment_text is not None:
-#             comments[commenter]["text"] = comment_text.text
-
-#         comment_link = comment.find(class_="_ns_")
-#         if comment_link is not None:
-#             comments[commenter]["link"] = comment_link.get("href")
-
-#         comment_pic = comment.find(class_="_2txe")
-#         if comment_pic is not None:
-#             comments[commenter]["image"] = comment_pic.find(class_="img").get("src")
-
-#         commentList = item.find('ul', {'class': '_7791'})
-#         if commentList:
-#             comments = dict()
-#             comment = commentList.find_all('li')
-#             if comment:
-#                 for litag in comment:
-#                     aria = litag.find("div", {"class": "_4eek"})
-#                     if aria:
-#                         commenter = aria.find(class_="_6qw4").text
-#                         comments[commenter] = dict()
-#                         comment_text = litag.find("span", class_="_3l3x")
-#                         if comment_text:
-#                             comments[commenter]["text"] = comment_text.text
-#                             # print(str(litag)+"\n")
-
-#                         comment_link = litag.find(class_="_ns_")
-#                         if comment_link is not None:
-#                             comments[commenter]["link"] = comment_link.get("href")
-
-#                         comment_pic = litag.find(class_="_2txe")
-#                         if comment_pic is not None:
-#                             comments[commenter]["image"] = comment_pic.find(class_="img").get("src")
-
-#                         repliesList = litag.find(class_="_2h2j")
-#                         if repliesList:
-#                             reply = repliesList.find_all('li')
-#                             if reply:
-#                                 comments[commenter]['reply'] = dict()
-#                                 for litag2 in reply:
-#                                     aria2 = litag2.find("div", {"class": "_4efk"})
-#                                     if aria2:
-#                                         replier = aria2.find(class_="_6qw4").text
-#                                         if replier:
-#                                             comments[commenter]['reply'][replier] = dict()
-
-#                                             reply_text = litag2.find("span", class_="_3l3x")
-#                                             if reply_text:
-#                                                 comments[commenter]['reply'][replier][
-#                                                     "reply_text"] = reply_text.text
-
-#                                             r_link = litag2.find(class_="_ns_")
-#                                             if r_link is not None:
-#                                                 comments[commenter]['reply']["link"] = r_link.get("href")
-
-#                                             r_pic = litag2.find(class_="_2txe")
-#                                             if r_pic is not None:
-#                                                 comments[commenter]['reply']["image"] = r_pic.find(
-#                                                     class_="img").get("src")
-#     return comments
-
-
-# def _extract_reaction(item):
-#     toolBar = item.find_all(attrs={"role": "toolbar"})
-
-#     if not toolBar:  # pretty fun
-#         return
-#     reaction = dict()
-#     for toolBar_child in toolBar[0].children:
-#         str = toolBar_child['data-testid']
-#         reaction = str.split("UFI2TopReactions/tooltip_")[1]
-
-#         reaction[reaction] = 0
-
-#         for toolBar_child_child in toolBar_child.children:
-
-#             num = toolBar_child_child['aria-label'].split()[0]
-
-#             # fix weird ',' happening in some reaction values
-#             num = num.replace(',', '.')
-
-#             if 'K' in num:
-#                 realNum = float(num[:-1]) * 1000
-#             else:
-#                 realNum = float(num)
-
-#             reaction[reaction] = realNum
-#     return reaction
-
-
-# def _extract_html(bs_data):
-
-#     #Add to check
-#     with open('./bs.html',"w", encoding="utf-8") as file:
-#         file.write(str(bs_data.prettify()))
-
-#     k = bs_data.find_all(class_="_5pcr userContentWrapper")
-#     postBigDict = list()
-
-#     for item in k:
-#         postDict = dict()
-#         postDict['Post'] = _extract_post_text(item)
-#         postDict['Link'] = _extract_link(item)
-#         postDict['PostId'] = _extract_post_id(item)
-#         postDict['Image'] = _extract_image(item)
-#         postDict['Shares'] = _extract_shares(item)
-#         postDict['Comments'] = _extract_comments(item)
-#         # postDict['Reaction'] = _extract_reaction(item)
-
-#         #Add to check
-#         postBigDict.append(postDict)
-#         with open('./postBigDict.json','w', encoding='utf-8') as file:
-#             file.write(json.dumps(postBigDict, ensure_ascii=False).encode('utf-8').decode())
-
-#     return postBigDict
-
-
 def _login(browser, email, password):
     browser.get("http://facebook.com")
     #browser.maximize_window()
@@ -248,9 +67,10 @@ def getNewCommentText(browser, setOfKnownComments, listOfComments):
     print(len(matches), "matches found ")
     for m in matches:
       try:
-        val = m.text
+        #val = m.text
         # print (m.text)
-        #print(m.find_element_by_xpath(".//div[contains(@style,'text-align: start;')]").text)
+        val = m.find_element_by_xpath(".//div[contains(@style,'text-align: start;')]").text
+        #print(val)
         if val not in setOfKnownComments:
           setOfKnownComments.add(val)
           listOfComments.append(val)
@@ -273,11 +93,9 @@ def saveResultsResults(path, browser, listOfComments):
     with open(path, mode='wt', encoding='utf-8') as myfile:
       for c in listOfComments:
         myfile.write(c)
-        myfile.write('\n\n')
+        myfile.write('\n')
       myfile.flush()
-    # Now that the page is fully scrolled, grab the source code.
     source_data = browser.page_source
-
     # Throw your source into BeautifulSoup and start parsing!
     bs_data = bs(source_data, 'html.parser')
     with open('./pagePartialSnapshot.html',"w", encoding="utf-8") as file:

c5e96604dbd5122218f55fa7b50e424c48196a66
Austin Stoker <austin@austinstoker.com>
2021-02-27 11:28:51 -0700

    got all comments open and identified

--- scraper.py
+++ scraper.py
@@ -2,11 +2,16 @@ import argparse
 import time
 import json
 import csv
+import sys
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup as bs
-
+# import Action chains  
+#from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.keys import Keys
 
 with open('facebook_credentials.txt') as file:
     EMAIL = file.readline().split('"')[1]
@@ -197,48 +202,62 @@ def _login(browser, email, password):
     browser.get("http://facebook.com")
     #browser.maximize_window()
     browser.find_element_by_name("email").send_keys(email)
-    #browser.find_element_by_name("pass").send_keys(password)
-    #browser.find_element_by_id('loginbutton').click()
-    time.sleep(30)
-    print("Ok hope you got your password input quickly")
+    browser.find_element_by_name("pass").send_keys(password)
+    browser.find_element_by_name('login').click()
 
-def _count_needed_scrolls(browser, infinite_scroll, numOfPost):
-    if infinite_scroll:
-        lenOfPage = browser.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;"
-        )
-    else:
-        # roughly 8 post per scroll kindaOf
-        lenOfPage = int(numOfPost / 8)
-    print("Number Of Scrolls Needed " + str(lenOfPage))
-    return lenOfPage
+def attemptClickByXpath(browser, desc, xpath):
+  keepTrying = True
+  while (keepTrying):
+    try:
+      print("searching for ",desc)
+      matches = browser.find_elements_by_xpath(xpath)
+      if not matches:
+        print("no match for ", desc)
+        return False
+      print("match found ", desc)
+      #browser.execute_script("return arguments[0].scrollIntoView(true);", matches[0])
+      matches[0].click()
+      time.sleep(1) #Todo do better here.
+      return True
+    except StaleElementReferenceException:
+      print("StaleElementReferenceException")
+    except NoSuchElementException:
+      print("NoSuchElementException")
+      return False
+    except:
+      print("Unhandled Exception")
+      e = sys.exc_info()[0]
+      print(e)
 
 
-def _scroll(browser, infinite_scroll, lenOfPage):
-    lastCount = -1
-    match = False
+def clickViewPreviousComments(browser):
+  return attemptClickByXpath(browser,"View previous comments", "//*[contains(text(), 'View previous comments')]")
 
-    while not match:
-        if infinite_scroll:
-            lastCount = lenOfPage
-        else:
-            lastCount += 1
+def expandComments(browser):
+  return attemptClickByXpath(browser,"See more","//*[contains(text(), 'See More')]")
 
-        # wait for the browser to load, this time can be changed slightly ~3 seconds with no difference, but 5 seems
-        # to be stable enough
-        time.sleep(5)
-
-        if infinite_scroll:
-            lenOfPage = browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return "
-                "lenOfPage;")
-        else:
-            browser.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return "
-                "lenOfPage;")
-
-        if lastCount == lenOfPage:
-            match = True
+def getNewCommentText(browser):
+  keepTrying = True
+  while (keepTrying):
+    try:
+      print("searching for Comments")
+      matches = browser.find_elements_by_xpath("//*[starts-with(@aria-label, 'Comment by')]")
+      if not matches:
+        print("no match for ")
+        return False
+      print("matches found ")
+      for m in matches:
+        print (m)
+      return True
+    except StaleElementReferenceException:
+      print("StaleElementReferenceException")
+    except NoSuchElementException:
+      print("NoSuchElementException")
+      return False
+    except:
+      print("Unhandled Exception")
+      e = sys.exc_info()[0]
+      print(e)
 
 def extract(page):
     option = Options()
@@ -254,9 +273,29 @@ def extract(page):
     # chromedriver should be in the same folder as file
     browser = webdriver.Chrome(executable_path="./chromedriver", options=option)
     _login(browser, EMAIL, PASSWORD)
+    time.sleep(5)
     browser.get(page)
     
     time.sleep(5)
+    howManyCommentPages = 0
+    howManyCommentsExpanded = 0
+    
+    getNewCommentText(browser)
+    while(clickViewPreviousComments(browser)):
+      howManyCommentPages += 1
+      while(expandComments(browser)):
+        howManyCommentsExpanded+=1
+        getNewCommentText(browser)
+
+      browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME)
+
+
+    print ("Pages of comments:")
+    print (howManyCommentPages)
+    print ("Comments expanded:")
+    print (howManyCommentsExpanded)
+    time.sleep(90)
+
     # Now that the page is fully scrolled, grab the source code.
     source_data = browser.page_source
 
